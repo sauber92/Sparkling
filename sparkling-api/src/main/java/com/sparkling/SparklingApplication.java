ba77612d2f60c51346b027d93f0c5d22ba7c151c
GAIN LEE <gain@GAINui-MacBook-Pro.local>
2016-12-25 18:00:54 +0900

    프로젝트 생성

---
+++ sparkling-api/src/main/java/com/sparkling/SparklingApplication.java
@@ -0,0 +1,12 @@
+package com.sparkling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparklingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparklingApplication.class, args);
+	}
+}
